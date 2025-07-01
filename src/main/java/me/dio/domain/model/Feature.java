c01cc67b669d229c252acfe6d2cf6a8051990ca7
MisaelFalcaoDev <misaelfalcao.dev@gmail.com>
2025-07-01 15:34:06 -0300

    #feat: mudanças nas exceções para português e adição do plugin lombok para evitar getters e setters

--- src/main/java/me/dio/domain/model/Feature.java
+++ src/main/java/me/dio/domain/model/Feature.java
@@ -1,7 +1,11 @@
 package me.dio.domain.model;
 
 import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
+@Data
 @Entity(name = "tb_feature")
 public class Feature extends BaseItem {
 
--- src/main/java/me/dio/domain/model/News.java
+++ src/main/java/me/dio/domain/model/News.java
@@ -1,7 +1,11 @@
 package me.dio.domain.model;
 
 import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
+@Data
 @Entity(name = "tb_news")
 public class News extends BaseItem {
 
